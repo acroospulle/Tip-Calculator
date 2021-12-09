5766ffd2bdcd492246e48aaebe453366f9bbe7a2
acroospulle <Ashni.Croospulle@cix.csi.cuny.edu>
2021-12-09 17:53:45 -0500

    removed pods

--- Pods/Target Support Files/Pods-TipCalculator-TipCalculatorUITests/Pods-TipCalculator-TipCalculatorUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TipCalculator_TipCalculatorUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TipCalculator_TipCalculatorUITests
-@end
--- Tip Calculator/CalculateTipViewController.swift
+++ Tip Calculator/CalculateTipViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class CalculateTipViewController: UIViewController {
 
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,6 +21,16 @@ class CalculateTipViewController: UIViewController {
     }
     
 
-  
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipAmountLabel.text = String (format: "$%.2f", tip)
+        totalLabel.text = String (format: "$%.2f", total)
+
+    }
+    
+    
 
 }
